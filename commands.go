bba3eaeee09f54c651fe924b2c4f33c232508ade
SlashNephy <spica@starry.blue>
2023-06-07 11:05:38 +0900

    複数行に対応

--- commands.go
+++ commands.go
@@ -25,6 +25,7 @@ var commands = []*DiscordCommand{
 			},
 		},
 		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) error {
+			// 「考え中」を出す
 			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
 			})
@@ -39,6 +40,7 @@ var commands = []*DiscordCommand{
 			debts := map[string][]DebtData{}
 			var messageRegex = regexp.MustCompile(`^(?:<@\d+>\s*)+([\d,]+)(?:\s*(.+))?$`)
 
+			// コマンドが実行されたチャンネルでメッセージを全件取得する
 			var page int
 			var beforeID string
 			for page < 5 {
@@ -56,31 +58,42 @@ var commands = []*DiscordCommand{
 						continue
 					}
 
-					match := messageRegex.FindStringSubmatch(m.Content)
-
-					var amount int
-					if len(match) >= 2 {
-						amount, err = strconv.Atoi(strings.Replace(match[1], ",", "", -1))
-						if err != nil {
-							slog.Warn("invalid amount", slog.String("content", m.Content))
+					// 改行ごとに借金フォーマットを探す
+					for _, line := range strings.Split(m.Content, "\n") {
+						match := messageRegex.FindStringSubmatch(line)
+						if len(match) == 0 {
 							continue
 						}
-					} else {
-						continue
-					}
 
-					var label string
-					if len(match) == 3 {
-						label = match[2]
-					}
+						// カンマ消す
+						s := strings.Replace(match[1], ",", "", -1)
 
-					slog.Info("found message", slog.Int("amount", amount), slog.String("label", label), slog.Any("mentions", m.Mentions))
+						// 借金の金額
+						amount, err := strconv.Atoi(s)
+						if err != nil {
+							slog.Warn("invalid amount", slog.String("content", line))
+							continue
+						}
 
-					for _, mention := range m.Mentions {
-						debts[mention.Mention()] = append(debts[mention.Mention()], DebtData{
-							Amount: amount,
-							Label:  label,
-						})
+						// 借金のラベル
+						var label string
+						if len(match) == 3 {
+							label = match[2]
+						}
+
+						slog.Info("found message",
+							slog.Int("amount", amount),
+							slog.String("label", label),
+							slog.Any("mentions", m.Mentions),
+							slog.String("content", line),
+						)
+
+						for _, mention := range m.Mentions {
+							debts[mention.Mention()] = append(debts[mention.Mention()], DebtData{
+								Amount: amount,
+								Label:  label,
+							})
+						}
 					}
 				}
 
@@ -100,6 +113,7 @@ var commands = []*DiscordCommand{
 				return err
 			}
 
+			// ユーザーごとに借金の合計を表示
 			var fields []string
 			for mention, data := range debts {
 				var sum int
