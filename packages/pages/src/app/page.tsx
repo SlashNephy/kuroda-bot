e2c7fd354d56cc39cfc425d08d36474bf3ac40f1
SlashNephy <spica@starry.blue>
2023-07-31 13:15:47 +0900

    create-app

---
+++ packages/pages/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const runtime = 'edge'
+
+export default function Home(): React.JSX.Element {
+  return (
+    <main>
+      <p>kuroda-bot</p>
+    </main>
+  )
+}
